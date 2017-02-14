a82cb6e34627e5d58990561aaf2c533c624cb68f
Nicholas McNew <Nicholas McNew>
2017-02-14 13:25:03 -0700

    Split up index for header and footer files, added worklist

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="master.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="main-nav">
+        <div id="user-block">
+            <div class="user-image">
+                <img src="icon-user.png" />
+            </div>
+            <h1>{Username}</h1>
+        </div>
+        <ul id="navbar">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="community.php">Community</a></li>
+            <li><a href="ranking.php">Ranks</a></li>
+            <li><a href="tourneys.php">Tourneys</a></li>
+        </ul>
+    </div>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+    include('header.php');
+
+    ?>
+    <div class="content">
+    <?php
+    //NOTE: possible to transport this in ajax
+    
+    $posts = array(
+        0 => array(
+            "type" => 1,
+            "poster-image" => "",
+            "Poster-ID" => "",
+            "poster-name" => "Nick",
+            "group-id" => "",
+            "group-name" => "test",
+            "text" => "Soufflé biscuit topping. Dessert chupa chups caramels cheesecake tiramisu cake chocolate cake. Bear claw candy cake lemon drops muffin candy chocolate cake. Marzipan soufflé gingerbread. Toffee candy canes dragée brownie gummi bears oat cake caramels chocolate cake fruitcake. Icing biscuit chupa chups powder soufflé croissant cotton candy wafer. Chocolate cake sweet roll bonbon pastry bonbon cake dessert tart caramels."
+            ),
+        1 => array(
+            "type" => 2,
+            "poster-image" => "",
+            "Poster-ID" => "",
+            "poster-name" => "Nick",
+            "group-id" => "",
+            "event-image" => "",
+            "group-name" => "test",
+            "text" => "Soufflé biscuit topping. Dessert chupa chups caramels cheesecake tiramisu cake chocolate cake. Bear claw candy cake lemon drops muffin candy chocolate cake. Marzipan soufflé gingerbread. Toffee candy canes dragée brownie gummi bears oat cake caramels chocolate cake fruitcake. Icing biscuit chupa chups powder soufflé croissant cotton candy wafer. Chocolate cake sweet roll bonbon pastry bonbon cake dessert tart caramels."
+            ),
+        );
+    #NOTE: takes in a list of posts and generates posts for them, can be used later in a function if need be
+    #could also rewrite in js and just pass the above information back from an ajax request
+    foreach($posts as $post){
+        echo '<div class="post ';
+        if ($post["type"] == 2)
+            echo 'event-post';
+        echo '">
+            <div class="poster-info">
+                <img class="poster-image" src="'. $post["poster-image"] .'" />
+                <a href="#" class="post-location">'. $post["group-name"] .'</a>
+                <a href="#" class="poster-name">'. $post["poster-name"] .'</a>
+            </div>
+            <div class="clear"></div>';
+        if ($post["type"] == 2)
+            echo '<img src="'.$post["event-image"].'" class="event-image" />';
+        echo '<div class="text">' . $post["text"] . '</div>
+            <a href="" class="button comment">Comment!</a>
+        </div>';
+    }
+    ?>
+    </div>
+<?php
+include('footer.php');
+?>
