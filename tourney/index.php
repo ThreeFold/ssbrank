9acb1c6b7e20e963833c529d5c1faafa164fd00c
Nicholas McNew <Nicholas McNew>
2017-02-22 16:27:02 -0700

    Added

---
+++ tourney/index.php
@@ -0,0 +1,18 @@
+<?php 
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/header.php";
+include_once($path);
+include_once("tourneyHeader.php");
+?>
+
+<div id="content">
+
+
+</div>
+
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/footer.php";
+include_once($path);
+
+?>
