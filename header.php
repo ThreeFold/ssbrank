4c14b04993dbb494355862e97f0efff1b34446cb
Nicholas McNew <Nicholas McNew>
2017-02-23 09:03:18 -0700

    fixed

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
         <ul id="navbar">
             <li><a href="/index.php">Home</a></li>
             <li><a href="/groups/">Community</a></li>
-            <li><a href="/ranking.php">Ranks</a></li>
+            <li><a href="/ranking/">Ranks</a></li>
             <li><a href="/tourney/">Tourneys</a></li>
         </ul>
     </div>
