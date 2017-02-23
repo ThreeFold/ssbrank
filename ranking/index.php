5b23c97d556acb2b0fd803aedbf5faf688a2228c
Nicholas McNew <Nicholas McNew>
2017-02-23 09:02:26 -0700

    added

---
+++ ranking/index.php
@@ -0,0 +1,16 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/header.php";
+
+include_once($path);
+include_once("rankHeader.php");
+?>
+
+
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/footer.php";
+
+include_once($path);
+
+?>
