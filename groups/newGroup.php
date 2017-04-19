af9302d0d78c650d15fef298663ccede7eaaedda
threefold <themickeynick@gmail.com>
2017-04-19 02:05:34 -0600

    s3 uploading?

--- groups/newGroup.php
+++ groups/newGroup.php
@@ -1,12 +1,12 @@
 <?php
 
-
 	session_start();
 	$path = $_SERVER['DOCUMENT_ROOT'];
     include_once($path . '/PDOFactory.php');
     include_once($path . '/classes/group.php');
     include_once($path . '/classes/user.php');
     include_once($path . '/library.php');
+    require $path. '/vendor/autoload.php';
 
 
 $name = clean($_POST['name']);
@@ -24,12 +24,15 @@ if(!preg_match('/^[a-zA-Z0-9_]{3,10}/', $region)){
 }
 $id = uniqid('', true);
 
+$target_dir = '';
+$rel = '';
+$header_image = '/rsc/images/default-header.png';
 if($_POST['name']){
 	$rel = "/groups/" . $id . '/';
 	$target_dir = $path . $rel;
 }
+$target_file = $target_dir . $_FILES['image']['name'];
 mkdir($target_dir, 0777, true);
-$target_file = $target_dir . basename($_FILES["image"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
@@ -37,18 +40,18 @@ $check = getimagesize($_FILES["image"]["tmp_name"]);
 if($check !== false) {
     $uploadOk = 1;
 } else {
-    $_SESSION['new_group_error'] .= "File is not an image.";
+    $_SESSION['new_group_error'] .= "<br/>File is not an image.";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["image"]["size"] > 500000) {
-    $_SESSION['new_group_error'] .=  "Sorry, your file is too large. Images must be < 500kB";
+    $_SESSION['new_group_error'] .=  "<br/>Sorry, your file is too large. Images must be < 500kB";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    $_SESSION['new_group_error'] .=  "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $_SESSION['new_group_error'] .=  "<br/>Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -56,16 +59,24 @@ if ($uploadOk == 0) {
 	header('Location:/groups/createGroup.php');
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-    } else {
+    $s3 = Aws\S3\S3Client::factory([
+        "region" => 'us-west-2',
+        'version'=> 'latest'
+        ]);
+    print_r(getenv());
+    $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+    try{
+        $upload = $s3->upload($bucket, 'groups/' . $id . '/' . $_FILES['image']['name'], fopen($_FILES['image']['tmp_name'], 'rb'), 'public-read');
+        $header_image = $upload->get('ObjectURL');
+    }
+     catch (Exception $e) {
         $_SESSION['new_group_error'] = "Sorry, there was an error uploading your file.";
     }
 }
 if(!empty($_SESSION['new_group_error']))
 	header('Location:/groups/createGroup.php');
 if($uploadOk){
-	$group = Community::newGroup($id, $name, $desc, $region, $rel . basename($_FILES["image"]["name"]));
+	$group = Community::newGroup($id, $name, $desc, $region, $header_image);
 	$group->addUser($_SESSION['user_id'], 5);
 	header('Location:/groups/group.php?name=' .$group->getName());
 }
