9dcf6506004b334113bda13e496634e6b266ae3d
= <=>
2017-04-19 18:35:20 -0600

    unset login

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
 	?>
 	<form action="enter_site.php" method="POST">
 		<h2>Basic Information:</h2>
-		<input class="text-input" type="text" name="email" required="TRUE" placeholder="Email" value="<?php echo $_SESSION['login-error']['email']?>" />
+		<input class="text-input" type="text" name="email" required="TRUE" placeholder="Email" value="<?php echo $_SESSION['login-error']['email']; unset($_SESSION['login-error']);?>" />
 		<input class="text-input" type="password" name="password" required="TRUE" placeholder="Password" />
 		<input class="landing-submit" type="submit" value="Submit">	
 		</form>
